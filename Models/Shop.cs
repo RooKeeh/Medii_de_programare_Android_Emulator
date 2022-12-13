772c4461ef23a91b77fd09a64efba3ed0b350ea1
Andrei Moldovan <andreimoldovan6@gmail.com>
2022-12-13 15:11:37 +0200

    Lab10

--- Models/Shop.cs
+++ Models/Shop.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLiteNetExtensions.Attributes;
 
 namespace Moldovan_Andrei_Lab7.Models
 {
